d486cf4948ef352751e2612ffd7a5c683dfd944d
El Mehdi Hakmoune (EXT) <ehakmoun@cegid.com>
2023-12-02 16:50:22 +0100

    Create error page 404

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import RequireAuth from './component/RequireAuth';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './component/Layout';
 import Contact from './component/contact/Contact';
+import NotFound from './component/404/NotFound';
 
 function App() {
   return (
@@ -16,11 +17,13 @@ function App() {
           <Route path='register' element={<Register />} />
         </Route>
 
-        {/* This route with path='*' will render the RequireAuth component for any URL that hasn't been matched by previous routes. */}
-        <Route path='*' element={<RequireAuth />}>
+        <Route element={<RequireAuth />}>
           {/** Routes that require authentication */}
           <Route path='contact' element={<Contact />} />
         </Route>
+
+        {/* This route with path='*' will render the NotFound component for any URL that hasn't been matched by previous routes. */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
---
+++ src/component/404/NotFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound = () => {
+    return (
+        <div className="home-title">
+            <h3>Not Found; Erro 404</h3>
+        </div>
+    )
+}
+
+export default NotFound;
